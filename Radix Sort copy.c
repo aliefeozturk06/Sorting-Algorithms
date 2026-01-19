d71d2530f6706fd5d785e8a0555ce9dc4b52f1c8
aliefeozturk06 <aliefeozturk06@gmail.com>
2026-01-20 01:12:46 +0300

    refactor: Radix Sort

---
+++ Radix Sort copy.c
@@ -0,0 +1,29 @@
+void countingSortDigit(int arr[], int n, int exp) {
+    int output[n];
+    int count[10] = {0};
+
+    for (int i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (int i = n - 1; i >= 0; i--) {
+        int digit = (arr[i] / exp) % 10;
+        output[count[digit] - 1] = arr[i];
+        count[digit]--;
+    }
+
+    for (int i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+void radixSort(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countingSortDigit(arr, n, exp);
+}
